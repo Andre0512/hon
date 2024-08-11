d76f3bddc7168406dbdb1af2cda74d9bc4b2dd5c
Kuba Sawulski <kuba@sawul.ski>
2024-08-11 11:29:09 +0200

    Fix mqtt Run black

--- custom_components/hon/__init__.py
+++ custom_components/hon/__init__.py
@@ -54,7 +54,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.unique_id] = {"hon": hon, "coordinator": coordinator}
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
+
     return True
 
 
